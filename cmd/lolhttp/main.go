3ea679679b5ac0e7833cf3c9a5bee5fe791db9ee
Jaspal Chauhan <jaspalch95@gmail.com>
2020-10-31 13:57:54 -0700

    Complete Ch.3: Create a socket, accept incoming connections

--- cmd/lolhttp/main.go
+++ cmd/lolhttp/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func handleErr(e error) {
+	if e == nil {
+		return
+	}
+	fmt.Fprintf(os.Stderr, "Error: %v\n", e)
+	os.Exit(1)
+}
 
 func main() {
-	fmt.Println("Hello world, it's lolhttp!")
+	address := "127.0.0.1"
+	port := "1456"
+
+	fmt.Println("Starting server on", net.JoinHostPort(address, port))
+	listener, err := net.Listen("tcp", net.JoinHostPort(address, port))
+	handleErr(err)
+
+	for {
+		_, err := listener.Accept()
+		handleErr(err)
+
+		fmt.Println("Accepted connection!")
+	}
 }
