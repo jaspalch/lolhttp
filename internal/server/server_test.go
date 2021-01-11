0e6bd3336593137a8764a10cead704783ae95bc3
Jaspal Chauhan <jaspalch95@gmail.com>
2021-01-10 18:52:05 -0800

    Ch. 7 Add more server tests, add handlers, utils tests

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -33,3 +33,56 @@ func TestNewServer(t *testing.T) {
 		})
 	}
 }
+
+func TestListen(t *testing.T) {
+	tests := []struct {
+		s    *Server
+		addr string
+		err  error
+	}{
+		{
+			s: &Server{
+				allowedMethods: []string{"GET"},
+			},
+			addr: "127.0.0.1",
+			err:  fmt.Errorf("No handler function set for method 'GET'"),
+		},
+		{
+			s:    NewServer(map[string]string{}),
+			addr: "bad address",
+			err:  fmt.Errorf("listen tcp: address bad address: missing port in address"),
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("test error: %v", tc.err), func(t *testing.T) {
+			err := tc.s.Listen(tc.addr)
+			assert.Equal(t, tc.err.Error(), err.Error())
+		})
+	}
+}
+
+func TestRegisterHandler(t *testing.T) {
+	tests := []struct {
+		method string
+		err    error
+	}{
+		{
+			method: "BAD",
+			err:    fmt.Errorf("Method 'BAD' is not a valid method"),
+		},
+		{
+			method: "GET",
+			err:    nil,
+		},
+	}
+
+	srv := NewServer(map[string]string{})
+
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("method to register: %v", tc.method), func(t *testing.T) {
+			err := srv.registerHandler(tc.method, func(s *Server, args []string) string { return "" })
+			assert.Equal(t, tc.err, err)
+		})
+	}
+}
